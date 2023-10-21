a63dc50b26cbc00140d8b3b32d2918b92e877c1e
Sebastian <seb154@live.com>
2023-10-21 16:18:01 -0400

    Adds .results.json and Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function getCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer; 
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'initialValue';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'newValue'; 
+}
+
+
+
